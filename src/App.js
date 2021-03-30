b435f7fe8119f54fa9f5f55cd18123a7bb7b3294
Artem Aleksieiev <artemaleksieiev@Artems-iMac.home>
2021-03-29 22:37:07 -0400

    adding package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React, { useState } from 'react';
 import ImageUploader from 'react-images-upload';
+import ReactFileReader from "react-file-reader";
 import Axios from 'axios';
 import './App.css';
-
-const UploadComponent = props => (
-    <form>
-    <ImageUploader
-        key="image-uploader"
-        withIcon={true}
-        singleImage={true}
-        withPreview={true}
-        label="Maximum size file: 5MB"
-        buttonText="Choose an image"
-        onChange={props.onImage}
-        imgExtension={['.jpg', '.pdf', '.doc']}
-        maxFileSize={5242880}
-    ></ImageUploader>
-  </form>
-);
 
 const App = () => {
   const [progress, setProgress] = useState('getUpload');
@@ -26,54 +11,63 @@ const App = () => {
   //const [url, setImageURL] = useState(undefined);
   const API_ENDPOINT = 'https://t39hcfjfn5.execute-api.us-east-2.amazonaws.com/uploads'
 
-  const onImage = async (failedImages, successImages) => {
+  const handleFiles = async (e) => {
+    console.log(e.base64)
     setProgress('uploading');
-    
     try {
-            console.log('successImages', successImages[0]);
-            console.log('Upload clicked');
-            // Get the presigned URL
-            const response = await Axios({
-              method: 'GET',
-              url: API_ENDPOINT
-            });
-            console.log('ResponseURL: ', response.data.uploadURL)
-            let binary = atob(successImages[0].split(',')[1])
-            let array = []
-            for (var i = 0; i < binary.length; i++) {
-                array.push(binary.charCodeAt(i))
-            }
-            console.log(array)
-            let blobData = new Blob([new Uint8Array(array)], {type: 'application/pdf'})
-            
-            console.log('Uploading to: ', response.data.uploadURL)
-            const result = await fetch(response.data.uploadURL, {
-              method: 'PUT',
-              body: blobData
-            })
-            console.log('Result: ', result)
-            console.log(response.data.uploadURL.split('?')[0])
-            setResumeLink(response.data.uploadURL.split('?')[0]);
-            setProgress('uploaded');
+          console.log('Upload clicked');
+          // Get the presigned URL
+          const response = await Axios({
+          method: 'GET',
+          url: API_ENDPOINT
+          });
+          console.log('ResponseURL: ', response.data.uploadURL)
+          let binary = atob(e.base64[0].split(',')[1])
+          let array = []
+          for (var i = 0; i < binary.length; i++) {
+              array.push(binary.charCodeAt(i))
+          }
+          console.log(array)
+          let blobData = new Blob([new Uint8Array(array)], {type: 'application/pdf'})
+          
+          console.log('Uploading to: ', response.data.uploadURL)
+          const result = await fetch(response.data.uploadURL, {
+            method: 'PUT',
+            body: blobData
+          })
+          console.log('Result: ', result)
+          console.log(response.data.uploadURL.split('?')[0])
+          setResumeLink(response.data.uploadURL.split('?')[0]);
+          setProgress('uploaded');
         } catch (error) {
             console.log('error in upload', error);
             setErrorMessage(error.message);
             setProgress('uploadError');
         }
-    };
-
+    
+  }
 
   const content = () => {
     switch (progress) {
         case 'getUpload':
-            return <UploadComponent onImage={onImage} />;
+            return (
+              <>
+                <div>
+                  <div>please upload a resume</div>
+                  <ReactFileReader fileTypes={[".pdf",".doc"]} base64={true} multipleFiles={true} handleFiles={(e)=>handleFiles(e)}>
+                    <button className='btn'>Upload</button>
+                  </ReactFileReader>
+                </div>
+              </>
+            );
         case 'uploading':
-            return <h2>Uploading....</h2>;
+            return <h2>Uploading....</h2>
         case 'uploaded':
             return (
               <>
-                <h2>Uploaded</h2>;
-                <h6>You can access resume by clicking this link: <a href={resumeLink}>{resumeLink}</a></h6>
+                <h2>Uploaded</h2>
+                <h6>You can access resume by clicking this link:</h6>
+                <a className='link' href={resumeLink}>{resumeLink}</a>
               </>
             );
         case 'uploadError':
